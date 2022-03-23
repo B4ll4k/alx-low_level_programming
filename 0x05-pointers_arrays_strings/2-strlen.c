aa2812010f1bdf40461c8cc31b56f4c3ed3d5bdf
Beamlak <beamlakalemseged@gmail.com>
2022-03-23 03:23:24 +0300

    pointers2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen - returns the length of the string from the parameter
+ * @s: string input
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '/0')
+		i++;
+	return i;
+}	
