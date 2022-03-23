cde4297c1251a681c3cc11bbff97539fc2260365
Beamlak <beamlakalemseged@gmail.com>
2022-03-23 04:13:00 +0300

    pointers4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "string.h"
+
+/**
+ * print_rev - prints the reversed string
+ * @s: the string to be reversed
+ */
+void print_rev(char *s)
+{
+	for (int i = strlen(s)-1; i >= 0; i--)
+		printf("%c", s[i]);
+	printf("\n");
+}
