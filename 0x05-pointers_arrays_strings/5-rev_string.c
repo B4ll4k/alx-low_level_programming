550d49576cca8c00ec0b4caee0cf6ec95ad77edf
Beamlak <beamlakalemseged@gmail.com>
2022-03-23 04:24:04 +0300

    pointers5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "string.h"
+
+/**
+ * rev_string - reverse the string
+ * @s: the string to be reversed
+ */
+void print_rev(char *s)
+{
+        int i;
+	int j = 0;
+	char *str;
+
+        for (i = strlen(s) - 1; i >= 0; i--)
+        {
+        	str[j] = s[i];
+			j++;
+	}
+        s = str;
+}
