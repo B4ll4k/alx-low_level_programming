dc33ce793d9dc7a20cd89add339fff7c47294b3e
Beamlak <beamlakalemseged@gmail.com>
2022-03-23 05:31:45 +0300

    pointers7-2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,14 @@ void puts_half(char *str)
 {
 	int n;
 	int i;
+	int l = strlen(str);
 
 	if (strlen(str) % 2 == 0)
 		n = strlen(str) / 2;
 	else
 		n = (strlen(str) - 1) / 2;
 
-	for (i = n; i < strlen(str); i++)
+	for (i = n; i < l; i++)
 	{
 		printf("%c", str[i]);
 	}
