535da89f02e40b9394da6ed0b4893a95b113744a
Beamlak <beamlakalemseged@gmail.com>
2022-03-23 03:01:33 +0300

    pointers010101

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
  * reset_to_98 - this function changes the value of the pointer passed to it.
- * @*n - integer pointer to the value to be changed
+ * @n: integer pointer to the value to be changed
  */
 void reset_to_98(int *n)
 {
