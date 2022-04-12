f95b6eec0fe192642ad87ef32eb5ce1babf9a221
Beamlak <beamlakalemseged@gmail.com>
2022-04-13 02:58:37 +0300

    D, E1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,8 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->age <= 0)
-		printf("Age: (nil)\n");
+	if (d->age <= 0 || d->age == NULL)
+		d->age = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
